53b55e0ff54f74fbcd36330c31b928df1e19902b
Chonwai <chonwaiun@gmail.com>
2022-11-29 14:21:58 +0800

    Update: Updated the User's name to nickname.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_14_165218) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_29_060556) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -194,7 +194,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_14_165218) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
+    t.string "nickname"
     t.string "phone"
     t.string "position"
     t.date "date_of_birth"
