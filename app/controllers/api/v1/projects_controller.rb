eb2747d65a3b8186129c3fcef51697b5733e02d0
Chonwai <chonwaiun@gmail.com>
2022-12-13 13:00:02 +0800

    Add: Added the user details on the show all project API.

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ProjectsController < ApiController
   before_action :authenticate_user!, only: [:create, :update, :destroy]
 
   def index
-    @projects = Project.all.includes([:project_tasks]).as_json(include: :project_tasks)
+    @projects = Project.all.includes([:project_tasks, :user]).as_json(include: [:project_tasks, :user])
     @projects = Kaminari.paginate_array(@projects).page(params[:page])
     render json: { success: true, projects: @projects, meta: pagination_meta(@projects) }, status: :ok
   end
