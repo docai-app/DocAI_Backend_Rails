39b91610acae84dbc7b8c7e98f9c001ae8bf2b65
Chonwai <chonwaiun@gmail.com>
2022-12-22 01:00:16 +0800

    Add: Added the pagination on the Drive APIs.

--- app/controllers/api/v1/drive_controller.rb
+++ app/controllers/api/v1/drive_controller.rb
@@ -4,15 +4,21 @@ class Api::V1::DriveController < ApiController
 
   def index
     @folders = Folder.where(parent_id: nil).includes(:user).as_json(include: { user: { only: [:id, :email, :nickname] } })
+    @folders = Kaminari.paginate_array(@folders).page(params[:page]).per(50)
     @documents = Document.where(folder_id: nil).includes(:user).as_json(except: [:label_list], include: { user: { only: [:id, :email, :nickname] } })
-    render json: { success: true, folders: @folders, documents: @documents }, status: :ok
+    @documents = Kaminari.paginate_array(@documents).page(params[:page]).per(50)
+    @meta = compare_pagination_meta(@folders, @documents)
+    render json: { success: true, folders: @folders, documents: @documents, meta: @meta }, status: :ok
   end
 
   def show
-    @folder = Folder.where(parent_id: params[:id]).includes(:user).as_json(include: { user: { only: [:id, :email, :nickname] } })
+    @folders = Folder.where(parent_id: params[:id]).includes(:user).as_json(include: { user: { only: [:id, :email, :nickname] } })
+    @folders = Kaminari.paginate_array(@folders).page(params[:page]).per(50)
     @ancestors = @current_user_folder.ancestors
     @documents = Document.where(folder_id: params[:id]).includes(:user).as_json(except: [:label_list], include: { user: { only: [:id, :email, :nickname] } })
-    render json: { success: true, folder: @current_user_folder, folders: @folder, ancestors: @ancestors, documents: @documents }, status: :ok
+    @documents = Kaminari.paginate_array(@documents).page(params[:page]).per(50)
+    @meta = compare_pagination_meta(@folders, @documents)
+    render json: { success: true, folder: @current_user_folder, folders: @folders, ancestors: @ancestors, documents: @documents, meta: @meta }, status: :ok
   end
 
   def share
@@ -34,5 +40,24 @@ class Api::V1::DriveController < ApiController
     else
       render json: { success: false, error: "You don't have permission to access this folder" }, status: :ok
     end
+  end
+
+  def compare_pagination_meta(object1, object2)
+    if pagination_meta(object1)[:total_pages] >= pagination_meta(object2)[:total_pages]
+      @meta = pagination_meta(object1)
+    else
+      @meta = pagination_meta(object2)
+    end
+    return @meta
+  end
+
+  def pagination_meta(object)
+    {
+      current_page: object.current_page,
+      next_page: object.next_page,
+      prev_page: object.prev_page,
+      total_pages: object.total_pages,
+      total_count: object.total_count,
+    }
   end
 end
