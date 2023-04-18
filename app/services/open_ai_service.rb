6089ce2ffbb9e49abc318b9b0e94aac3c9935013
Chonwai <chonwaiun@gmail.com>
2023-04-18 19:05:55 +0800

    Update: Updated the prompt and the formatter.

--- app/services/open_ai_service.rb
+++ app/services/open_ai_service.rb
@@ -5,7 +5,7 @@ class OpenAiService
   def self.chatWithDocument(query, content, response_format, language, topic, style)
     client = OpenAI::Client.new(access_token: @access_token)
     # prompt = "Now you are an efficient assistant and you have a mission is about writting an #{style} style #{format} by using #{language}. #{query}, The output should follow the format \'\'\'json\n {\"content\": \"\"} \'\'\'!!! and the text content is as follows: #{content}"
-    prompt = "You are currently an assistant, and there is a task based on document content generation that requires your assistance. Please help me generate a #{response_format} content about #{topic} using #{language}, and generate it in #{style} style. The output should follow the format \'\'\'json\n {\"content\": \"\"} \'\'\'! The task is as follows: #{query}, with the reference document as follows: #{content}."
+    prompt = "You are currently an assistant, and there is a task based on document content generation that requires your assistance. The generated output json must be follow the format: \'\'\'json\n {\"content\": \"\"} \'\'\'!!! Please help me generate a #{response_format} content about #{topic} using #{language}, and generate it in #{style} style. The generated output json must be follow the format: \'\'\'json\n {\"content\": \"\"} \'\'\'!!! The task is as follows: #{query}, with the reference document as follows: #{content}."
     response = client.chat(
       parameters: {
         model: "gpt-3.5-turbo", # Required.
@@ -17,7 +17,7 @@ class OpenAiService
         presence_penalty: 0,
       },
     )
-    puts response["choices"][0]["message"]["content"]
+    puts "response: #{response["choices"][0]["message"]["content"]}"
     content = Utils.cleansingContentFromGPT(response["choices"][0]["message"]["content"])
     return content
   end
--- app/utils/utils.rb
+++ app/utils/utils.rb
@@ -1,16 +1,15 @@
-require "JSON"
+require "json"
 
 class Utils
   def self.cleansingContentFromGPT(content)
-    json_regex = /{.*}/m
+    json_regex = /{[\s\S]*?}/m
     json_match = content.match(json_regex)
 
-    puts "json_match: #{json_match}"
-
     if json_match
-      json_str = json_match.to_s.gsub('\n', '\\n')
+      json_str = json_match.to_s
       puts "json_str: #{json_str}"
       json_obj = JSON.parse(json_str)
+      puts "json_obj: #{json_obj}"
       return json_obj
     else
       puts "No JSON found in the paragraph"
