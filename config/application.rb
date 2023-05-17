b82cf69bb5a7eab7f4cc330a82d01519baf76b8e
Chonwai <chonwaiun@gmail.com>
2023-05-17 15:17:27 +0800

    Update: Updated the application setting.

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative "boot"
 
 require "rails/all"
 require 'dotenv/load'
+require 'apartment/elevators/subdomain'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -36,6 +37,9 @@ module DocaiApi
     config.api_only = true
 
     config.active_job.queue_adapter = :sidekiq
+
+    # config.middleware.use Apartment::Elevators::Domain
+    config.middleware.use Apartment::Elevators::Subdomain
 
     Redis.exists_returns_integer = true
   end
