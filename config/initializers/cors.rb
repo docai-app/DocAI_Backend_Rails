e240d0bbad93e31048a8f5a80a029d83962b0e24
Chonwai <chonwaiun@gmail.com>
2023-05-17 15:00:53 +0800

    Update: Updated the CORS value.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "http://localhost:8080", "https://docai.m2mda.com", "https://docai-dev.m2mda.com", "https://doc-ai-dev-frontend.vercel.app", "http://doc-ai-dev-frontend.vercel.app", "https://doc-ai-frontend.vercel.app", "http://doc-ai-frontend.vercel.app", "https://chyb.docai.net", "http://chyb.docai.net", "https://*-dev.docai.net", "http://*-dev.docai.net", "https://chyb-dev.docai.net/", "http://chyb-dev.docai.net/", "https://wishcoffee-dev.docai.net/", "http://wishcoffee-dev.docai.net/"
+    origins "http://localhost:3000", "http://localhost:8080", "https://docai.m2mda.com", "https://docai-dev.m2mda.com", "https://doc-ai-dev-frontend.vercel.app", "http://doc-ai-dev-frontend.vercel.app", "https://doc-ai-frontend.vercel.app", "http://doc-ai-frontend.vercel.app", "https://chyb.docai.net", "http://chyb.docai.net", "https://*-dev.docai.net", "http://*-dev.docai.net", "https://chyb-dev.docai.net", "http://chyb-dev.docai.net", "https://wishcoffee-dev.docai.net", "http://wishcoffee-dev.docai.net"
 
     resource "*",
       headers: :any,
