aa778b0c138775531822cf24c40a77d957f2b797
Chonwai <chonwaiun@gmail.com>
2023-05-18 13:05:50 +0800

    Update: Updated the application setting.

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -26,7 +26,7 @@ Apartment.configure do |config|
   # - a hash which keys are tenant names, and values custom db config
   # (must contain all key/values required in database.yml)
   #
-  config.tenant_names = lambda{ Customer.pluck(:tenant_name) }
+  # config.tenant_names = lambda{ Customer.pluck(:tenant_name) }
   config.tenant_names = ['chyb-dev', 'wishcoffee-dev']
   # config.tenant_names = {
   #   'tenant1' => {
