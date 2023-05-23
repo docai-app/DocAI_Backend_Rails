ed5bbf5be66810363057da091a42f0840aa7b1af
Chonwai <chonwaiun@gmail.com>
2023-05-23 14:07:52 +0800

    Update: Updated the Application Controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::API
-    before_action :set_paper_trail_whodunnit
+  include Devise::Controllers::Helpers
+  before_action :set_paper_trail_whodunnit, switch_tenant
+
+  def switch_tenant
+    subdomain = Utils.extractReferrerSubdomain(request.referrer)
+
+    if Apartment.tenant_names.include?(subdomain)
+      Apartment::Tenant.switch!(subdomain)
+    else
+      Apartment::Tenant.switch!("public")
+    end
+  end
 end
