7b849c94a94bf841f3468285e25b65fd6fd9fab2
Chonwai <chonwaiun@gmail.com>
2023-05-23 14:10:45 +0800

    Update: Updated the Application Controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
   include Devise::Controllers::Helpers
-  before_action :set_paper_trail_whodunnit, switch_tenant
+  before_action :set_paper_trail_whodunnit
+  before_action :switch_tenant
 
   def switch_tenant
     subdomain = Utils.extractReferrerSubdomain(request.referrer)
