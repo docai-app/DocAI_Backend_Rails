16c3c4ee7ca4fdd49c0cf37753ff0e33c4c9f985
Chonwai <chonwaiun@gmail.com>
2023-11-23 23:58:24 +0800

    Run the rubocop formatter.

--- db/migrate/20231116060842_add_airflow_accepted_to_dag_run.rb
+++ db/migrate/20231116060842_add_airflow_accepted_to_dag_run.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddAirflowAcceptedToDagRun < ActiveRecord::Migration[7.0]
   def change
     add_column :dag_runs, :airflow_accepted, :boolean, default: false, null: false
