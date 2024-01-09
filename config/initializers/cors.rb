f05bf732606fbd470207863ad0e13f26fbc64485
Chonwai <chonwaiun@gmail.com>
2024-01-09 14:07:00 +0800

    Update: Updated the cors.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,6 +9,21 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
+    origins 'http://localhost:3000', 'http://localhost:8080', 'https://docai.m2mda.com', 'https://docai-dev.m2mda.com', 'https://docai-demo.examhero.com', 'http://docai-demo.examhero.com', 'https://aiadmin.examhero.com', 'http://aiadmin.examhero.com',
+            'https://doc-ai-dev-frontend.vercel.app', 'http://doc-ai-dev-frontend.vercel.app', 'https://doc-ai-frontend.vercel.app', 'http://doc-ai-frontend.vercel.app', 'https://docai-demo.vercel.app', 'http://docai-demo.vercel.app', 'https://aiadmin.docai.net', 'http://aiadmin.docai.net',
+            'https://test-dev.docai.net', 'http://test-dev.docai.net', 'https://test.docai.net', 'http://test.docai.net',
+            'https://chatbot-demo.docai.net', 'http://chatbot-demo.docai.net', 'https://chatbot.docai.net', 'http://chatbot.docai.net', 'https://prod-docai-chatbot.vercel.app', 'http://prod-docai-chatbot.vercel.app', 'https://dev-docai-chatbot.vercel.app', 'http://dev-docai-chatbot.vercel.app', 'https://docai-chatbot-next.vercel.app', 'http://docai-chatbot-next.vercel.app', 'https://chatbot-dev.docai.net', 'http://chatbot-dev.docai.net', 'https://dev-docai-chatbot-plus.vercel.app', 'http://dev-docai-chatbot-plus.vercel.app/',
+            'https://chyb.docai.net', 'http://chyb.docai.net', 'https://*-dev.docai.net', 'http://*-dev.docai.net',
+            'https://chyb-dev.docai.net', 'http://chyb-dev.docai.net', 'https://chyb.docai.net', 'http://chyb.docai.net', 'https://chyb-dev.docai.net:8080', 'http://chyb-dev.docai.net:8080',
+            'https://wishcoffee-dev.docai.net', 'http://wishcoffee-dev.docai.net', 'https://wishcoffee.docai.net', 'http://wishcoffee.docai.net',
+            'https://docai-dev.docai.net', 'http://docai-dev.docai.net', 'https://docai.docai.net', 'http://docai.docai.net',
+            'https://hku-dev.docai.net', 'http://hku-dev.docai.net', 'https://hku.docai.net', 'http://hku.docai.net',
+            'https://mastercorp-dev.docai.net', 'http://mastercorp-dev.docai.net', 'https://mastercorp.docai.net', 'http://mastercorp.docai.net',
+            'https://xinhua-dev.docai.net', 'http://xinhua-dev.docai.net', 'https://xinhua.docai.net', 'http://xinhua.docai.net',
+            'https://mjsse-dev.docai.net', 'http://mjsse-dev.docai.net', 'https://mjsse.docai.net', 'http://mjsse.docai.net',
+            'https://phc-dev.docai.net', 'http://phc-dev.docai.net', 'https://phc.docai.net', 'http://phc.docai.net'
+
+    # origins Cors.all.pluck(:url)
     origins *Cors.pluck(:url)
 
     resource '*',
