d2a6debc5d058ebe9299c731f080fd2546d3e7ce
Chonwai <chonwaiun@gmail.com>
2024-02-21 17:23:25 +0800

    Add: Add ClassificationModelVersion model and its associated files

---
+++ app/models/classification_model_version.rb
@@ -0,0 +1,2 @@
+class ClassificationModelVersion < ApplicationRecord
+end
---
+++ test/models/classification_model_version_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ClassificationModelVersionTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
