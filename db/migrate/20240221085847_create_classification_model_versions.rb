f3803a405d09ffb6ea2f1813ad6a1d5a87795276
Chonwai <chonwaiun@gmail.com>
2024-02-21 17:24:12 +0800

    Run the formatter.

--- db/migrate/20240221085847_create_classification_model_versions.rb
+++ db/migrate/20240221085847_create_classification_model_versions.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateClassificationModelVersions < ActiveRecord::Migration[7.0]
   def change
     create_table :classification_model_versions, id: :uuid do |t|
