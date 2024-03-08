9f460956fba5adc48737aa6254032292d5edad5a
Chonwai <chonwaiun@gmail.com>
2024-03-09 02:32:55 +0800

    Refactor: Refactor document classification job and add error logging

--- app/sidekiq/document_classification_job.rb
+++ app/sidekiq/document_classification_job.rb
@@ -3,60 +3,79 @@
 class DocumentClassificationJob
   include Sidekiq::Worker
 
-  sidekiq_options retry: 3, dead: true, queue: 'document_classification', throttle: { threshold: 1, period: 10.second }
+  sidekiq_options retry: 3, dead: true, queue: 'document_classification', throttle: { threshold: 1, period: 10.seconds }
 
-  sidekiq_retry_in { |count| 60 * 60 * 1 * count }
+  sidekiq_retry_in { |count| 1.hour * count }
 
-  sidekiq_retries_exhausted do |msg, _ex|
-    _message = "error: #{msg['error_message']}"
+  sidekiq_retries_exhausted do |msg|
+    puts "Job retries exhausted: #{msg['error_message']}"
   end
 
   def perform(tenant)
+    Apartment::Tenant.switch!(tenant) do
+      puts "====== Tenant: #{tenant} ======"
+      documents_to_classify = fetch_documents_to_classify
+      if documents_to_classify.count > 5
+        view_name = create_classification_view(tenant, Time.now.to_i)
+        if train_model(tenant, view_name) && predict_first_document(tenant, view_name, documents_to_classify)
+          update_documents_as_trained(documents_to_classify)
+          puts '====== Documents were successfully processed for classification ======'
+        end
+      else
+        puts '====== No document needs classification ======'
+      end
+    end
+  rescue StandardError => e
+    log_error(e.message)
+  end
+
+  private
+
+  def fetch_documents_to_classify
+    # @documents = Document.classifiable.not_classified_as_document.to_train.retry_limit.order(created_at: :desc)
     @documents = Document.where(is_classified: true).where.not(content: nil).where.not(content: '').where('LENGTH(content) > ?', 10).where(is_document: true).where(is_classifier_trained: false).where(
       'retry_count < ?', 3
     ).order('created_at': :desc)
-    Apartment::Tenant.switch!(tenant)
-    timestamp = Time.now.to_i
-    # document = Document.find(document_id)
-    # if document.present? && document.is_document && document.content.present? && !document.is_classifier_trained
-    #   classificationRes = RestClient.post "#{ENV['DOCAI_ALPHA_URL']}/classification/confirm",
-    #                                       { content: document.content, label: label_id, model: subdomain }.to_json, { content_type: :json, accept: :json }
-    #   if JSON.parse(classificationRes)['status']
-    #     document.is_classified = true
-    #     document.is_classifier_trained = true
-    #     document.confirmed!
-    #   else
-    #     document.retry_count += 1
-    #     document.error_message = JSON.parse(classificationRes)
-    #     document.save!
-    #   end
-    # end
-    if @documents.present? && @documents.count > 5
-      sql = "CREATE VIEW public.\"document_classification_model_#{timestamp}\" AS SELECT documents.id, documents.content, tag_id FROM \"#{tenant}\".documents INNER JOIN \"#{tenant}\".taggings ON \"#{tenant}\".documents.id = \"#{tenant}\".taggings.taggable_id WHERE \"#{tenant}\".documents.is_classified = true AND \"#{tenant}\".documents.retry_count < 3;"
-      puts "====== sql: #{sql} ======"
-      ActiveRecord::Base.connection.execute(sql)
-      res = RestClient.post "#{ENV['DOCAI_ALPHA_URL']}/classification/retrain",
-                            { model: tenant, viewName: "document_classification_model_#{timestamp}" }.to_json, { content_type: :json, accept: :json }
-      puts "====== res: #{res} ======"
-      if JSON.parse(res)['status'] == 'Success'
-        ClassificationModelVersion.create(
-          classification_model_name: "#{tenant}_document_classification_model_#{timestamp}",
-          entity_name: tenant,
-          pervious_version_id: ClassificationModelVersion.where(entity_name: tenant).order(created_at: :desc).first&.id || nil
-        )
-        @documents.update_all(is_classifier_trained: true)
-      end
-      puts '====== perform ====== documents was successfully processed'
-    else
-      puts '====== perform ====== no document needs classification ======'
-    end
-  rescue StandardError => e
-    # document = Document.find(document_id)
-    # document.retry_count += 1
-    # document.error_message = e.message
-    # document.save!
-    # puts "====== error ====== document.id: #{document_id}"
-    # puts "Document Classification processing failed for document #{document_id}: #{e.message}"
-    puts "====== error ====== error: #{e.message}"
+    puts "====== Documents found: #{@documents.count} ======"
+    @documents
+  end
+
+  def create_classification_view(tenant, timestamp)
+    sql = <<-SQL
+      CREATE VIEW public."document_classification_model_#{timestamp}" AS#{' '}
+      SELECT documents.id, documents.content, tag_id#{' '}
+      FROM "#{tenant}".documents#{' '}
+      INNER JOIN "#{tenant}".taggings ON "#{tenant}".documents.id = "#{tenant}".taggings.taggable_id#{' '}
+      WHERE "#{tenant}".documents.is_classified = true#{' '}
+      AND "#{tenant}".documents.retry_count < 3;
+    SQL
+
+    puts "SQL for view creation: #{sql}"
+    ActiveRecord::Base.connection.execute(sql)
+    "document_classification_model_#{timestamp}"
+  end
+
+  def train_model(tenant, view_name)
+    res = RestClient.post("#{ENV['DOCAI_ALPHA_URL']}/classification/retrain",
+                          { model: tenant, viewName: view_name }.to_json,
+                          { content_type: :json, accept: :json })
+    JSON.parse(res)['status'] == 'Success'
+  end
+
+  def predict_first_document(tenant, view_name, documents)
+    document_content = documents.where('LENGTH(content) < ?', 100).first.content.to_s
+    predict_res = RestClient.get("#{ENV['DOCAI_ALPHA_URL']}/classification/predict",
+                                 params: { content: URI.encode_www_form_component(document_content),
+                                           model: "#{tenant}_#{view_name}" })
+    JSON.parse(predict_res)['status'] == 'Success'
+  end
+
+  def update_documents_as_trained(documents)
+    ClassificationModelVersion.create_new_version(documents, tenant)
+    documents.update_all(is_classifier_trained: true)
+  end
+
+  def log_error(message)
+    puts "====== Error during document classification job: #{message} ======"
   end
 end
