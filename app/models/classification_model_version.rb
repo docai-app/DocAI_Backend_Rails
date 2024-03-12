825dd2493c7d04fd481eb25155b087638b258b20
Chonwai <chonwaiun@gmail.com>
2024-03-12 19:11:52 +0800

    Run the annotate models.

--- app/models/classification_model_version.rb
+++ app/models/classification_model_version.rb
@@ -2,16 +2,21 @@
 
 # == Schema Information
 #
-# Table name: public.classification_model_versions
+# Table name: classification_model_versions
 #
-#  id                  :uuid             not null, primary key
-#  model_name          :string           not null
-#  entity_name         :string           not null
-#  description         :string           default("")
-#  pervious_version_id :uuid
-#  meta                :jsonb
-#  created_at          :datetime         not null
-#  updated_at          :datetime         not null
+#  id                        :uuid             not null, primary key
+#  classification_model_name :string           not null
+#  entity_name               :string           not null
+#  description               :string           default("")
+#  pervious_version_id       :uuid
+#  meta                      :jsonb
+#  created_at                :datetime         not null
+#  updated_at                :datetime         not null
+#
+# Indexes
+#
+#  index_classification_model_versions_on_entity_name          (entity_name)
+#  index_classification_model_versions_on_pervious_version_id  (pervious_version_id)
 #
 class ClassificationModelVersion < ApplicationRecord
 end
--- app/models/dag_run.rb
+++ app/models/dag_run.rb
@@ -14,11 +14,13 @@
 #  created_at       :datetime         not null
 #  updated_at       :datetime         not null
 #  airflow_accepted :boolean          default(FALSE), not null
+#  tanent           :string
 #
 # Indexes
 #
 #  index_dag_runs_on_airflow_accepted  (airflow_accepted)
 #  index_dag_runs_on_dag_status        (dag_status)
+#  index_dag_runs_on_tanent            (tanent)
 #  index_dag_runs_on_user_id           (user_id)
 #
 # Foreign Keys
