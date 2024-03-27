4f1fee5858500ceaf026a06edb39e01be46d6860
Chonwai <chonwaiun@gmail.com>
2024-03-27 13:49:40 +0800

    Add: Add polymorphic association and has_one association to ScheduledTask model

---
+++ app/controllers/api/v1/scheduled_tasks_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class ScheduledTasksController < ApplicationController
+    end
+  end
+end
--- app/models/scheduled_task.rb
+++ app/models/scheduled_task.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class ScheduledTask < ApplicationRecord
+  belongs_to :user, polymorphic: true
+  has_one :entity
 end
