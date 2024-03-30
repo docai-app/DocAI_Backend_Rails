566b15fa2eab774269745ecbe660e5393bac3455
loveson821 <loveson821@gmail.com>
2024-03-30 15:57:39 +0800

    保存做題記錄計分

--- app/models/assessment_record.rb
+++ app/models/assessment_record.rb
@@ -18,5 +18,29 @@
 #  index_assessment_records_on_recordable  (recordable_type,recordable_id)
 #
 class AssessmentRecord < ApplicationRecord
+  
+  store_accessor :meta, :final_score
+  store_accessor :record, :questions
+
   belongs_to :recordable, polymorphic: true
+
+  after_create :calculate_score
+
+  def calculate_score
+    # 初始化分数
+    score = 0
+
+    # 遍历所有问题
+    questions.each do |question|
+      # 比较正确答案和用户答案
+      if question["answer"] == question["user_answer"]
+        # 如果答案正确，分数增加1
+        score += 1
+      end
+    end
+
+    # 返回最终分数
+    self['meta']['final_score'] = score
+    save
+  end
 end
