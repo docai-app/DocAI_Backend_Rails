91b786f0bd2e5640eef3fdf1a49f130bc80973da
Chonwai <chonwaiun@gmail.com>
2024-05-02 19:11:25 +0800

    Update: Add timezone column to general_users table in schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_240_502_103_901) do
+ActiveRecord::Schema[7.0].define(version: 20_240_502_110_101) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -404,6 +404,7 @@ ActiveRecord::Schema[7.0].define(version: 20_240_502_103_901) do
     t.integer 'sex'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.string 'timezone', default: 'Asia/Hong_Kong', null: false
     t.index ['email'], name: 'index_general_users_on_email', unique: true
   end
 
