c5fff775a58bc8b85d2d047881cbc7a36817baec
loveson821 <loveson821@gmail.com>
2025-04-01 17:03:22 +0800

    speaking pronunciation grading score api

--- app/controllers/api/v1/essay_assignments_controller.rb
+++ app/controllers/api/v1/essay_assignments_controller.rb
@@ -81,7 +81,7 @@ module Api
               eg['score'] = overall_score
             elsif @essay_assignment.speaking_pronunciation?
               # sb_score = eg.calculate_speaking_pronunciation_score
-              self['full_score'] = eg['grading']['speaking_pronunciation_sentences'].count
+              eg['full_score'] = eg['grading']['speaking_pronunciation_sentences'].count
               # eg['score'] = 1
               # binding.pry
             else
