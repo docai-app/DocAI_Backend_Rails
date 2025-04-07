a2d8e124f5b4419668f5bf11fd078e2febaf3948
loveson821 <loveson821@gmail.com>
2025-04-07 22:08:51 +0800

    sentence builder advance

--- app/services/sentence_builder_example_service.rb
+++ app/services/sentence_builder_example_service.rb
@@ -9,7 +9,7 @@ class SentenceBuilderExampleService
   def initialize(user_id, essay_assignment)
     @user_id = user_id
     
-    if essay_assignment.rubric == 'advanced'
+    if essay_assignment.rubric['name'] == 'Sentence Builder Advanced'
       @app_key = ENV['sentence_builder_example_app_key_advanced']
     else
       @app_key = ENV['sentence_builder_example_app_key']
