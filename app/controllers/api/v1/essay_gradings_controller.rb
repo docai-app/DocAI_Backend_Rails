2074512447f3acf97fc0ad4fef1e2ba43f475dda
loveson821 <loveson821@gmail.com>
2025-04-09 19:56:52 +0800

    add supplementPracticeService

--- app/controllers/api/v1/essay_gradings_controller.rb
+++ app/controllers/api/v1/essay_gradings_controller.rb
@@ -5,7 +5,7 @@ require 'prawn/table'
 module Api
   module V1
     class EssayGradingsController < ApiController
-      # before_action :authenticate_general_user!, except: [:download_report]
+      before_action :authenticate_general_user!, except: [:download_report]
 
       def download_report
         set_essay_grading
