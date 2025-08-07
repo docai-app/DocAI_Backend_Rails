7eeca34271d3f65be6bf9910c400a99b6682fbc0
聪 <505810824@qq.com>
2025-08-07 21:30:21 +0800

    hide ahoy.track

--- app/controllers/api/v1/essay_gradings_controller.rb
+++ app/controllers/api/v1/essay_gradings_controller.rb
@@ -276,9 +276,9 @@ module Api
         if @essay_grading.save
           # Track assignment submission
           # 首先，確保 Ahoy tracker 與當前提交作業的用戶正確關聯
-          ahoy.authenticate(current_general_user) if current_general_user
-          ahoy.track 'Assignment Submitted',
-                     { essay_grading_id: @essay_grading.id, essay_assignment_id: @essay_assignment.id }
+        #   ahoy.authenticate(current_general_user) if current_general_user
+        #   ahoy.track 'Assignment Submitted',
+        #              { essay_grading_id: @essay_grading.id, essay_assignment_id: @essay_assignment.id }
           render json: { success: true, essay_grading: @essay_grading }, status: :created
         else
           render json: { success: false, errors: @essay_grading.errors.full_messages }, status: :unprocessable_entity
